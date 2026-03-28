934c21ba848c54d25c08b35e992cef7b12e13c69
interacao <42029438+interacao@users.noreply.github.com>
2026-03-28 17:05:09 -0300

    Delete src directory

--- src/pages/Index.tsx
+++
@@ -1,9 +0,0 @@
-const Index = () => {
-  return (
-    <div className="min-h-screen bg-background flex items-center justify-center">
-      <span style={{ fontSize: '10px' }} className="text-foreground">teste</span>
-    </div>
-  );
-};
-
-export default Index;
